f4880dca6b68c93415b1baf6efacaa8e2b429708
Pedro Trindade <pedrito4315@gmail.com>
2025-05-01 22:21:18 +0100

    frontend

---
+++ Frontend/website/src/Components/Team/Team.jsx
@@ -0,0 +1,10 @@
+import "../Team/Team.css"
+
+const Team = () => {
+    return (
+        <section className="team-container" id="team">
+            <h1 className="team-title">Equipa</h1>
+        </section>
+    )
+}
+export default Team;
