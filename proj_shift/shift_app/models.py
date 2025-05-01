f4c96cd48dc451f830e181691c4bcf962520ebdd
João Vaz <joao.vaz1810@gmail.com>
2025-05-01 18:56:17 +0100

    setup do django

---
+++ proj_shift/shift_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Cliente(models.Model):
+    nome_completo = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+    idade = models.PositiveIntegerField(validators=[MinValueValidator(0),MaxValueValidator(150)])
+    morada = models.CharField(max_length=200)
+    observacoes = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f"{self.nome_completo} ({self.email})"
