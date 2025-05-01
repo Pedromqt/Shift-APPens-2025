ca844403e6c06c8967200d1b0640a7c3d0d52b05
Pedro Trindade <pedrito4315@gmail.com>
2025-05-01 17:58:17 +0100

    detetar pessoas e objetos

--- Backend/CamDetection/camDetection.py
+++ Backend/CamDetection/camDetection.py
@@ -1,5 +1,8 @@
 import cv2
+from ultralytics import YOLO
 
+# Carrega o modelo (yolov5s.pt deve estar no mesmo diretório)
+model = YOLO("yolov5s.pt")
 
 def capture():
     cam = cv2.VideoCapture(0)
@@ -7,18 +10,25 @@ def capture():
     if not cam.isOpened():
         print("Cam não abriu")
         exit()
-        
+
     while True:
         ret, frame = cam.read()
-
         if not ret:
-            print("tira")
-            
-        cv2.imshow("asda",frame)
-        
-        if (cv2.waitKey(5) > 0):
+            print("Erro ao capturar frame")
             break
+
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Realiza a detecção de objetos
+        results = model(img_rgb)  # Rodar a inferência
+
+        # A renderização do resultado será no formato de uma lista, então pegamos o primeiro
+        annotated_frame = results[0].plot()  # Renderiza a imagem com as caixas de detecção
+        cv2.imshow("Detecção", annotated_frame)
         
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
     cam.release()
     cv2.destroyAllWindows()
 
